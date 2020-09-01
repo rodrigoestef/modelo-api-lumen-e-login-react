fc537cbaa726c9191ad760201465fdd1a73f76ff
rodrigoestef <rodrigoestef@gmail.com>
2020-09-01 09:53:25 -0300

    formulario de nova senha

--- src/router/unauthorizated.js
+++ src/router/unauthorizated.js
@@ -2,12 +2,14 @@ import  React  from "react";
 import { BrowserRouter,Switch,Route ,Redirect} from "react-router-dom";
 import Login from '../routes/login'
 import NewUser from '../routes/newUser'
+import NewPass from '../routes/newPass'
 
 export default ()=>(
     <BrowserRouter>
         <Switch>
             <Route exact path='/login' component={Login}/>
             <Route exact path='/novoUsuario' component={NewUser}/>
+            <Route exact path='/novasenha' component={NewPass}/>
             <Route path='/' render={() => <Redirect to='/login'/>}/>
         </Switch>
     </BrowserRouter>
--- src/routes/newPass/index.js
+++ src/routes/newPass/index.js
@@ -3,15 +3,13 @@ import qs from 'querystring';
 import conexao from '../../conexao';
 import { useHistory } from 'react-router-dom';
 
-export default ()=>{
-    const [name,setName] = useState('')
-    const [email,setEmail] = useState('')
+export default () =>{
+    const [token,setToken] = useState('')
     const [password,setPassword] = useState('')
     const [password2,setPassword2] = useState('')
     const history = useHistory()
-
-    const  cadastrar = async() =>{
-        if (name == '' || email == '' || password == '') {
+    const redefinir = async() =>{
+        if (token == '' || password == '') {
             return
         }
         if (password2 != password) {
@@ -19,8 +17,7 @@ export default ()=>{
             return
         }
         const con = conexao()
-        const {data} = await con.post('/user/newuser',qs.stringify({name,email,password}))
-        
+        const {data} = await con.put('/user/newpass',qs.stringify({token,password}))
         if (data.failed) {
             alert(data.failed)
         }
@@ -29,16 +26,12 @@ export default ()=>{
             history.push('/')
         }
     }
-
     return(
         <div class='container'>
             <div class='p-5'></div>
             <div class='col-md-4 m-auto bg-white shadow-lg p-3 text-center rounded'>
                 <div class='form-group'>
-                    <input class='form-control' value={name} onChange={e=>setName(e.target.value)} placeholder='primeiro nome'/>
-                </div>
-                <div class='form-group'>
-                    <input class='form-control' type='email' value={email} onChange={e=>setEmail(e.target.value)} placeholder='seu@email.com'/>
+                    <input class='form-control' value={token} onChange={e=>setToken(e.target.value)} placeholder='código'/>
                 </div>
                 <div class='form-group'>
                     <input class='form-control' type='password' value={password} onChange={e=>setPassword(e.target.value)} placeholder='senha'/>
@@ -47,7 +40,7 @@ export default ()=>{
                     <input class='form-control' type='password' value={password2} onChange={e=>setPassword2(e.target.value)} placeholder='confirmar senha'/>
                 </div>
                 <div class='form-group'>
-                    <button class='btn btn-info col' onClick={()=>{cadastrar()}}>cadastar</button>
+                    <button class='btn btn-info col' onClick={() => redefinir()}>redefinir</button>
                 </div>
                 <div class='form-group'>
                 </div>
